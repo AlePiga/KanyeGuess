1391deea55d3be3330300d36ac6afe952cdde1d4
Alessandro Pigaiani <134812981+AlePiga@users.noreply.github.com>
2024-11-04 08:29:03 +0100

    Added bold text to results

--- KanyeGuess.js
+++ KanyeGuess.js
@@ -153,9 +153,9 @@ function getSong(act){
   let message = "";
 
   // Se la canzone
-  if (act == 0) { message = `<i>Wrong! The song was "${songTitle}".</i>`; } 
-  else if (act == 1) { message = `<i>The song you just skipped was "${songTitle}".</i>`; } 
-  else if (act == 2) { playE(); message = `<i>Correct! The song was "${songTitle}".</i>`; cnt++; }
+  if (act == 0) { message = `<i>Wrong! The song was <b>"${songTitle}".</b></i>`; } 
+  else if (act == 1) { message = `<i>The song you just skipped was <b>"${songTitle}".</b></i>`; } 
+  else if (act == 2) { playE(); message = `<i>Correct! The song was <b>"${songTitle}".</b></i>`; cnt++; }
 
   document.getElementById("result").innerHTML = message;
 }
